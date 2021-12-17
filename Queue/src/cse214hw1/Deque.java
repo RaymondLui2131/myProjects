9820272e13b1296cac07d6dbb2143d15e24c7b5d
Raymond Lui <86009783+RaymondLui2131@users.noreply.github.com>
2021-12-17 02:42:51 -0500

    Delete Queue directory

--- Queue/src/cse214hw1/Deque.java
+++
@@ -1,8 +0,0 @@
-package cse214hw1;
-
-public interface Deque<T> {
-    void addFirst(T t);
-    void addLast(T t);
-    T removeFirst();
-    T removeLast();
-}
--- Queue/src/cse214hw1/Queue.java
+++
@@ -1,9 +0,0 @@
-package cse214hw1;
-
-public interface Queue<T> {
-    void add(T t);
-    T remove();
-    T peek();
-
-
-}
