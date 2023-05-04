17d637194910bfe0d261f880a8ebcfd7b6e044cf
Raymond Lui <86009783+RaymondLui2131@users.noreply.github.com>
2023-05-03 21:34:44 -0400

    Delete src directory

--- src/components/newestButton.js
+++
@@ -1,6 +0,0 @@
-import SortQuestionByDate from '../components/sortQuestionByDate'
-
-export default function NewestButton ({ questions }) {
-  const sortedArr = SortQuestionByDate(questions)
-  return sortedArr
-}
